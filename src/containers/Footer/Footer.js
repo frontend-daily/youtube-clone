8b8c61745b5e11ae2b7088d1b6d50e5c4d725d41
pascal <pascal@marascientific.org>
2022-07-29 10:13:00 +0300

    Add assets and folderstructure

---
+++ src/containers/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BottomNav from '../../components/BottomNav/BottomNav'
+
+const Footer = () => {
+  return (
+    <div>
+        <BottomNav />
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/containers/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SecondNav from '../../components/SecondNav/SecondNav'
+import TopNav from '../../components/TopNav/TopNav'
+
+const Header = () => {
+  return (
+    <div>
+        <TopNav />
+        <SecondNav />
+    </div>
+  )
+}
+
+export default Header
