6b2e51a66483b257bb60c69a068d865ad23d9897
faith <faith@marascientific.org>
2022-08-08 20:02:38 +0300

    Add top nav bar

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import youtube from "../../assets/youtube.svg"
+import {AiOutlineSearch} from 'react-icons/ai'
+import {BiMicrophone,BiVideoPlus} from 'react-icons/bi'
+import {IoMdNotificationsOutline} from 'react-icons/io'
+import {FaUserCircle} from 'react-icons/fa'
+import "./TopNav.scss"
+
+
 
 const TopNav = () => {
   return (
-    <div>TopNav</div>
+      <nav className='top-nav'>
+      <ul>
+        <li><img className='logo' src={youtube} alt="youtube icon"/></li>
+        <li><AiOutlineSearch/></li>
+        <li><BiMicrophone/></li>
+        <li><BiVideoPlus/></li>
+        <li><IoMdNotificationsOutline/></li>
+        <li><FaUserCircle/></li>
+      </ul>
+      </nav>
+   
   )
 }
 
